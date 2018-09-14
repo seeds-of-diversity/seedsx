b574f26439f9b1d9999e6e403363fd670385fe4f
bobwildfong <bob@seeds.ca>
2018-09-13 22:16:30 -0400

    small changes to archive.php

--- seeds.ca2/app/csci/archive.php
+++ seeds.ca2/app/csci/archive.php
@@ -18,7 +18,6 @@ $oApp = new SEEDAppConsole(
                            )
 );
 
-$oApp->kfdb->SetDebug(1);
 $o = new SLDBCollection( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
 $ra = $o->GetList( 'IxGxAxPxS', "I._key BETWEEN 1000 and 1010" );
 echo SEEDCore_ArrayExpandRows( $ra, "<p>[[inv_number]] [[P_name]]</p>" );
@@ -29,8 +28,10 @@ $ra = $o->GetList( 'IxA_P', "I._key BETWEEN 1000 and 1010" );
 echo SEEDCore_ArrayExpandRows( $ra, "<p>[[_key]] [[P_name]]</p>" );
 
 echo "<hr/";
+
+$oApp->kfdb->SetDebug(2);
 $o = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
-$ra = $o->GetList( 'SRCCVxSRC', "SRCCV._key BETWEEN 14750 and 14760" );
+$ra = $o->GetList( 'SRCCVxSRC_P', "SRCCV._key BETWEEN 14750 and 14760" );
 echo SEEDCore_ArrayExpandRows( $ra, "<p>[[_key]] [[osp]] [[ocv]] [[SRC_name_en]]</p>" );
 
 
