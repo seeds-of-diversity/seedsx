3bacfb4271c4ac329c65a647a4fede4005e2cdfe
bobwildfong <bob@seeds.ca>
2018-09-19 21:47:38 -0400

    show species list from two relations

--- seeds.ca2/app/csci/archive.php
+++ seeds.ca2/app/csci/archive.php
@@ -20,12 +20,14 @@ $oApp = new SEEDAppConsole(
 
 $oApp->kfdb->SetDebug(1);
 
+
+$s = "";
+
+
+/*
 $oSrc = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
 $raCompanies = $oSrc->GetList( "SRC",     "", array("iStatus"=>-1, "sSortCol"=>'SRC.name_en' ) );
 $raSpecies   = $oSrc->GetList( "SRCCVxS", "SRCCV.fk_sl_sources>='3'", array('sGroupCols'=>'S_name_en,S__key',"sSortCol"=>'S.name_en' ) );
-
-
-$s = "";
 
 $s .= "<form method='post'>"
      ."<div class='container' style='border:1px solid #aaa;border-radius:5px;'>"
@@ -53,10 +55,58 @@ $s .= "<form method='post'>"
      ."</form>";
 
 
+$kCompany = SEEDInput_GetInt( 'company' );
+$kSpecies = SEEDInput_GetInt( 'species' );
+
+if( !$kCompany && !$kSpecies ) {
+    $s = "<p>Choose a company and/or a species before searching. "
+        ."Otherwise you would get the entire seed finder archive, which is too many results all at once.</p>";
+    goto done;
+}
+
+$cond = array();
+if( $kCompany ) {
+    $cond[] = "SRCCV.fk_sl_sources='$kCompany'";
+}
+if( $kSpecies ) {
+    $cond[] = "SRCCV.fk_sl_sp"
+}
+*/
+
+//$oApp->kfdb->SetDebug(2);
+$oSrc = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
+// where fk_sl_species is set -- iStatus=-1 because some sl_sources will be "deleted"
+$raSpecies1 = $oSrc->GetList( "SRCCVAxSRC_S", "SRC._key>='3' AND S._key IS NOT NULL",
+                              array('sGroupCols'=>'S_name_en,year','iStatus'=>-1 ) );
+// where fk_sl_species is zero
+$raSpecies2 = $oSrc->GetList( "SRCCVA", "fk_sl_sources>='3' AND osp <> ''",
+                              array('sGroupCols'=>'osp') );
+$raSp = array();
+foreach( $raSpecies1 as $ra ) {
+    if( !isset( $raSp[$ra['S_name_en']] ) ) {
+        $raSp[$ra['S_name_en']] = array('kSL'=>true,'name'=>$ra['S_name_en'],'raYears'=>array());
+    }
+    $raSp[$ra['S_name_en']]['raYears'][$ra['year']] = true;
+}
+foreach( $raSpecies2 as $ra ) {
+    if( !isset( $raSp[$ra['osp']] ) ) {
+        $raSp[$ra['osp']] = array('kSL'=>false,'name'=>"<span style='color:orange'>{$ra['osp']}</span>",'raYears'=>array());
+    }
+    $raSp[$ra['osp']]['raYears'][$ra['year']] = true;
+}
+ksort( $raSp );
+
+foreach( $raSp as $ra ) {
+    $raYears = array_keys($ra['raYears']);
+    $s .= "<p>".$ra['name']." ".(count($raYears) ? SEEDCore_MakeRangeStr($raYears) : "")."</p>";
+}
+
+
+done:
 
 echo Console02Static::HTMLPage( $s, "", 'EN', array('sCharset'=>'cp1252') );
 
-
+/*
 $o = new SLDBCollection( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
 $ra = $o->GetList( 'IxGxAxPxS', "I._key BETWEEN 1000 and 1010" );
 echo SEEDCore_ArrayExpandRows( $ra, "<p>[[inv_number]] [[P_name]]</p>" );
@@ -71,5 +121,6 @@ echo "<hr/";
 $o = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
 $ra = $o->GetList( 'SRCCVxSRC_P', "SRCCV._key BETWEEN 14750 and 14760" );
 echo SEEDCore_ArrayExpandRows( $ra, "<p>[[_key]] [[osp]] [[ocv]] [[SRC_name_en]]</p>" );
+*/
 
 ?>
