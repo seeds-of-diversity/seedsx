f76a320fb9ad5a246a86f8692ab6991240646ae7
bobwildfong <bob@seeds.ca>
2018-09-28 21:01:28 -0400

    factor archive.php code into a class

--- seeds.ca2/app/csci/archive.php
+++ seeds.ca2/app/csci/archive.php
@@ -25,7 +25,7 @@ $s = "";
 
 
 /*
-$oSrc = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
+$oSrc = new SLDBSources( $oApp );
 $raCompanies = $oSrc->GetList( "SRC",     "", array("iStatus"=>-1, "sSortCol"=>'SRC.name_en' ) );
 $raSpecies   = $oSrc->GetList( "SRCCVxS", "SRCCV.fk_sl_sources>='3'", array('sGroupCols'=>'S_name_en,S__key',"sSortCol"=>'S.name_en' ) );
 
@@ -74,37 +74,74 @@ if( $kSpecies ) {
 */
 
 //$oApp->kfdb->SetDebug(2);
-$oSrc = new SLDBSources( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
-// where fk_sl_species is set -- iStatus=-1 because some sl_sources will be "deleted"
-$raSpecies1 = $oSrc->GetList( "SRCCVAxSRC_S", "SRC._key>='3' AND S._key IS NOT NULL",
-                              array('sGroupCols'=>'S_name_en,year','iStatus'=>-1 ) );
-// where fk_sl_species is zero
-$raSpecies2 = $oSrc->GetList( "SRCCVA", "fk_sl_sources>='3' AND osp <> ''",
-                              array('sGroupCols'=>'osp') );
-$raSp = array();
-foreach( $raSpecies1 as $ra ) {
-    if( !isset( $raSp[$ra['S_name_en']] ) ) {
-        $raSp[$ra['S_name_en']] = array('kSL'=>true,'name'=>$ra['S_name_en'],'raYears'=>array());
-    }
-    $raSp[$ra['S_name_en']]['raYears'][$ra['year']] = true;
-}
-foreach( $raSpecies2 as $ra ) {
-    if( !isset( $raSp[$ra['osp']] ) ) {
-        $raSp[$ra['osp']] = array('kSL'=>false,'name'=>"<span style='color:orange'>{$ra['osp']}</span>",'raYears'=>array());
-    }
-    $raSp[$ra['osp']]['raYears'][$ra['year']] = true;
-}
-ksort( $raSp );
 
+
+$o = new QSRCCVA( $oApp );
+$raSp = $o->GetSpecies();
+
+$raYears = $oApp->kfdb->QueryRA( "SELECT year FROM seeds.sl_cv_sources_archive WHERE fk_sl_sources>='3' AND _status='0' GROUP BY 1 ORDER BY 1" );
+
+$s .= "<table>";
 foreach( $raSp as $ra ) {
-    $raYears = array_keys($ra['raYears']);
-    $s .= "<p>".$ra['name']." ".(count($raYears) ? SEEDCore_MakeRangeStr($raYears) : "")."</p>";
+    $name = $ra['kSp'] ? "{$ra['name']} ({$ra['kSp']})" : "<span style='color:orange'>{$ra['name']}</span>";
+    $s .= "<tr><td>$name</td>";
+    $s .= "<td>".(count($ra['raYears']) ? SEEDCore_MakeRangeStr($ra['raYears']) : "")."</td>";
+    $s .= "</tr>";
 }
-
+$s .= "</table>";
 
 done:
 
 echo Console02Static::HTMLPage( $s, "", 'EN', array('sCharset'=>'cp1252') );
+
+class QSRCCVA
+{
+    private $oApp;
+
+    function __construct( SEEDAppSession $oApp )
+    {
+        $this->oApp = $oApp;
+        $this->oSrc = new SLDBSources( $oApp );
+    }
+
+    function GetSpecies( $raParms = array() )
+    {
+        // Until all of the archive's species names are known in Rosetta, some SRCCVA rows will have fk_sl_species and some won't.
+        // This unifies those sets of names.
+
+        $raSpecies = array();
+
+        // Get names where fk_sl_species is set. iStatus=-1 because some sl_sources will be "deleted"
+        $raSp1 = $this->oSrc->GetList( "SRCCVAxSRC_S", "SRC._key>='3' AND S._key IS NOT NULL",
+                                       array('sGroupCols'=>'S_name_en,S__key,year','iStatus'=>-1 ) );
+        // Where fk_sl_species is zero
+        $raSp2 = $this->oSrc->GetList( "SRCCVA", "fk_sl_sources>='3' AND osp <> ''",
+                                       array('sGroupCols'=>'osp,year') );
+
+        foreach( $raSp1 as $ra ) {
+            // S_name_en and S__key should be correlated 1:1, but several rows can different years.
+            if( !isset( $raSpecies[$ra['S_name_en']] ) ) {
+                $raSpecies[$ra['S_name_en']] = array('kSp'=>$ra['S__key'],'name'=>$ra['S_name_en'],'raYears'=>array());
+            }
+            $raSpecies[$ra['S_name_en']]['raYears'][$ra['year']] = true;
+        }
+        foreach( $raSp2 as $ra ) {
+            // Several rows can be returned with the same osp but different years.
+            if( !isset( $raSpecies[$ra['osp']] ) ) {
+                $raSpecies[$ra['osp']] = array('kSp'=>0,'name'=>$ra['osp'],'raYears'=>array());
+            }
+            $raSpecies[$ra['osp']]['raYears'][$ra['year']] = true;
+        }
+        // Sort the array by name, and sort the years within each species (years have been stored as keys to simplify uniqueness but change them to values).
+        ksort( $raSpecies );    // sort by name
+        foreach( $raSpecies as $k => $ra ) {
+            $raSpecies[$k]['raYears'] = array_keys($ra['raYears']);
+            sort($raSpecies[$k]['raYears']);
+        }
+
+        return( $raSpecies );
+    }
+}
 
 /*
 $o = new SLDBCollection( $oApp, array( 'logdir'=>SITE_LOG_ROOT ) );
