8cdf2f9943fe6c3ecbe1cb0cdda3077d64d6dba5
bobwildfong <bob@seeds.ca>
2018-10-19 08:15:58 -0400

    new SEEDMailerSetup

---
+++ seeds.ca2/office/mbr/mailsetup.php
@@ -0,0 +1,38 @@
+<?php
+
+if( !defined("SITEROOT") )  define("SITEROOT", "../../");
+include_once( SITEROOT."site.php" );
+include_once( SEEDCORE."console/console02.php" );
+
+include_once( SEEDLIB."mail/SEEDMailer.php" );
+
+$oApp = new SEEDAppConsole(
+                array_merge( $SEEDKFDB1,
+                             array( 'sessPermsRequired' => array(),
+                                    'logdir' => SITE_LOG_ROOT )
+                           )
+);
+
+$oMail = new SEEDMailerSetup( $oApp );
+
+$sMailTable = "";
+$sPreview = "";
+$sControls = ""; // $oConsole->TabSetDraw( "right" )
+
+
+$s = "<table border='0' cellspacing='0' cellpadding='10' width='100%'><tr>"
+    ."<td valign='top'>"
+        ."<form method='post' action='${_SERVER['PHP_SELF']}'>"
+        //.SEEDForm_Hidden( "p_kMail", $oMS->kMail )
+        .$sMailTable
+        ."</form>"
+        .$sPreview
+    ."</td>"
+    ."<td valign='top' style='border-left:solid grey 1px;padding-left:2em;width:50%'>"
+        .$sControls
+    ."</td>"
+    ."</tr></table>";
+
+echo Console02Static::HTMLPage( utf8_encode($s), "", 'EN', array() );   // sCharset defaults to utf8
+
+?>
