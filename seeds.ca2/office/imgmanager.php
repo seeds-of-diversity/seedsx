bb613ceeb7ddbc1e981c1ae6df31c9381a6a154c
bobwildfong <bob@seeds.ca>
2018-10-20 22:05:48 -0400

    imgmanager new

---
+++ seeds.ca2/office/imgmanager.php
@@ -0,0 +1,16 @@
+<?php
+
+if( !defined("SITEROOT") )  define("SITEROOT", "../");
+include_once( SITEROOT."site2.php" );
+include_once( SEEDAPP."imgman/ImgManager.php" );
+
+$oApp = new SEEDAppConsole(
+                array_merge( $SEEDKFDB2,
+                             array( 'sessPermsRequired' => array(), //  'imgman' => 'W' ),
+                                    'logdir' => SITE_LOG_ROOT )
+                           )
+);
+
+ImgManagerApp( $oApp );
+
+?>
