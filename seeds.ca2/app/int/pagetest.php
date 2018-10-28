748c620fb53c70f50504630c792b8463698ec7c3
bobwildfong <bob@seeds.ca>
2018-10-28 14:52:28 -0400

    use ConsolePage class

--- seeds.ca2/app/int/pagetest.php
+++ seeds.ca2/app/int/pagetest.php
@@ -3,25 +3,30 @@
 if( !defined("SITEROOT") )  define("SITEROOT", "../../");
 include_once( SITEROOT."site.php" );
 
-$s .= "
+$s = "
 <div id='consolePage0'>
+<form onsubmit='oCP.PageSubmit();return false;'>
 <p>Question 1: What's your name?</p>
 <input type='text' id='myname'/>
-<button onclick='consolePageSubmit()'>Next</button>
-</div>
+<input type='submit' value='Next'/>
+</form></div>
 
 <div id='consolePage1' style='display:none'>
+<form onsubmit='oCP.PageSubmit();return false;'>
 <div>Your name is <span id='myname'></span></div>
 <p>Question 2: Who's your cat?</p>
 <input type='text' id='mycat'/>
-<button onclick='consolePageSubmit()'>Next</button>
+<input type='submit' value='Next'/>
+</form>
 </div>
 
 <div id='consolePage2' style='display:none'>
+<form onsubmit='oCP.PageSubmit();return false;'>
 <p>Confirm</p>
 <div>Your name is <span id='myname'></span></div>
 <div>Your cat is <span id='mycat'></span></div>
-<input type='submit' onclick='consolePageSubmit()'/>
+<input type='submit'/>
+</form>
 </div>
 ";
 
@@ -29,17 +34,16 @@ $s .= "
 
 $s .= "
 <script>
-ConsolePageStart( { nMaxPage: 2,
-                    vars: {},
-                    fns: {
+var config = {
+                    pages: {
                         0: {
-                             pre: function() {},
-                             post: function() {
-                                 var name = $('#consolePage0 #myname').val();
-                                 var p = 0;
+                             fnPre: function() {},
+                             fnPost: function() {
+                                 let name = $('#consolePage0 #myname').val();
+                                 let p = 0;
 
                                  if( name != '' ) {
-                                     consolePageObj['vars']['name'] = name;
+                                     oCP.SetVar('name',name);
                                      p = 1;
                                  }
 
@@ -47,15 +51,15 @@ ConsolePageStart( { nMaxPage: 2,
                              }
                            },
                         1: {
-                             pre: function() {
-                                 $('#consolePage1 #myname').html(consolePageObj['vars']['name']);
+                             fnPre: function() {
+                                 $('#consolePage1 #myname').html(oCP.GetVar('name'));
                              },
-                             post: function() {
-                                 var cat = $('#consolePage1 #mycat').val();
-                                 var p = 1;
+                             fnPost: function() {
+                                 let cat = $('#consolePage1 #mycat').val();
+                                 let p = 1;
 
                                  if( cat != '' ) {
-                                     consolePageObj['vars']['cat'] = cat;
+                                     oCP.SetVar('cat',cat);
                                      p = 2;
                                  }
 
@@ -63,21 +67,23 @@ ConsolePageStart( { nMaxPage: 2,
                              }
                            },
                         2: {
-                             pre: function() {
-                                 $('#consolePage2 #myname').html(consolePageObj['vars']['name']);
-                                 $('#consolePage2 #mycat').html(consolePageObj['vars']['cat']);
+                             fnPre: function() {
+                                 $('#consolePage2 #myname').html(oCP.GetVar('name'));
+                                 $('#consolePage2 #mycat').html(oCP.GetVar('cat'));
                              },
-                             post: function() {
+                             fnPost: function() {
                                  finalReport();
                                  return( 2 );
                              },
                            }
                     }
-} );
+};
+
+var oCP = new ConsolePage( config );
 
 function finalReport()
 {
-    alert( 'You are '+consolePageObj['vars']['name']+' and your cat is '+consolePageObj['vars']['cat'] );
+    alert( 'You are '+oCP.GetVar('name')+' and your cat is '+oCP.GetVar('cat') );
 }
 
 </script>";
