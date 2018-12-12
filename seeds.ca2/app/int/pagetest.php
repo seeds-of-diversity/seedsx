41bb8bbf38c0da78c99030bbbfbe0b8508e470d3
bobwildfong <bob@seeds.ca>
2018-12-12 13:44:39 -0500

    consolePage plateau

--- seeds.ca2/app/int/pagetest.php
+++ seeds.ca2/app/int/pagetest.php
@@ -7,15 +7,15 @@ $s = "
 <div id='consolePage0'>
 <form onsubmit='oCP.PageSubmit();return false;'>
 <p>Question 1: What's your name?</p>
-<input type='text' id='myname'/>
+<input type='text' class='cpvar_name'/>
 <input type='submit' value='Next'/>
 </form></div>
 
 <div id='consolePage1' style='display:none'>
 <form onsubmit='oCP.PageSubmit();return false;'>
-<div>Your name is <span id='myname'></span></div>
+<div>Your name is <span class='cpvar_name'></span></div>
 <p>Question 2: Who's your cat?</p>
-<input type='text' id='mycat'/>
+<input type='text' class='cpvar_cat'/>
 <input type='submit' value='Next'/>
 </form>
 </div>
@@ -23,8 +23,8 @@ $s = "
 <div id='consolePage2' style='display:none'>
 <form onsubmit='oCP.PageSubmit();return false;'>
 <p>Confirm</p>
-<div>Your name is <span id='myname'></span></div>
-<div>Your cat is <span id='mycat'></span></div>
+<div>Your name is <span class='cpvar_name'></span></div>
+<div>Your cat is <span class='cpvar_cat'></span></div>
 <input type='submit'/>
 </form>
 </div>
@@ -35,48 +35,52 @@ $s = "
 $s .= "
 <script>
 var config = {
-                    pages: {
-                        0: {
-                             fnPre: function() {},
-                             fnPost: function() {
-                                 let name = $('#consolePage0 #myname').val();
-                                 let p = 0;
+        pages: {
+            0: {
+                 fnPre: function() {},
+                 fnPost: function() {
+                     let name = oCP.FormVal( 0, 'name' );
+                     let p = 0;
 
-                                 if( name != '' ) {
-                                     oCP.SetVar('name',name);
-                                     p = 1;
-                                 }
+                     if( name != '' ) {
+                         //oCP.SetVar('name',name);
+                         oCP.StoreVars(0);
+                         p = 1;
+                     }
 
-                                 return( p );
-                             }
-                           },
-                        1: {
-                             fnPre: function() {
-                                 $('#consolePage1 #myname').html(oCP.GetVar('name'));
-                             },
-                             fnPost: function() {
-                                 let cat = $('#consolePage1 #mycat').val();
-                                 let p = 1;
+                     return( p );
+                 }
+               },
+            1: {
+                 fnPre: function() {
+                     oCP.LoadVars(1);
+                     //$('#consolePage1 .cpvar_name').html(oCP.GetVar('name'));
+                 },
+                 fnPost: function() {
+                     let cat = oCP.FormVal( 1, 'cat' ); //$('#consolePage1 .cpvar_cat').val();
+                     let p = 1;
 
-                                 if( cat != '' ) {
-                                     oCP.SetVar('cat',cat);
-                                     p = 2;
-                                 }
+                     if( cat != '' ) {
+                         //oCP.SetVar('cat',cat);
+                         oCP.StoreVars(1);
+                         p = 2;
+                     }
 
-                                 return( p );
-                             }
-                           },
-                        2: {
-                             fnPre: function() {
-                                 $('#consolePage2 #myname').html(oCP.GetVar('name'));
-                                 $('#consolePage2 #mycat').html(oCP.GetVar('cat'));
-                             },
-                             fnPost: function() {
-                                 finalReport();
-                                 return( 2 );
-                             },
-                           }
-                    }
+                     return( p );
+                 }
+               },
+            2: {
+                 fnPre: function() {
+                     oCP.LoadVars(2);
+                     //$('#consolePage2 .cpvar_name').html(oCP.GetVar('name'));
+                     //$('#consolePage2 .cpvar_cat').html(oCP.GetVar('cat'));
+                 },
+                 fnPost: function() {
+                     finalReport();
+                     return( 2 );
+                 },
+               }
+        }
 };
 
 var oCP = new ConsolePage( config );
