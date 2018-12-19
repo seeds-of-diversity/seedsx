8cb8a679fa6713ae764cef74572f7f3e49598469
bobwildfong <bob@seeds.ca>
2018-12-18 21:17:31 -0500

    control all imgmanlib config from the app

--- seeds.ca2/office/imgmanager.php
+++ seeds.ca2/office/imgmanager.php
@@ -4,14 +4,14 @@ if( !defined("SITEROOT") )  define("SITEROOT", "../");
 include_once( SITEROOT."site2.php" );
 include_once( SEEDAPP."imgman/ImgManager.php" );
 
-$oApp = new SEEDAppConsole(
-                array_merge( $config_KFDB['seeds2'],
-                             array( 'sessPermsRequired' => array(), //  'imgman' => 'W' ),
-                                    'logdir' => SITE_LOG_ROOT )
-                           )
+$oApp = new SEEDAppConsole( $config_KFDB['seeds2'] + [ 'sessPermsRequired' => array(), //  'imgman' => 'W' ),
+                                                       'logdir' => SITE_LOG_ROOT ]
 );
 
-ImgManagerApp( $oApp, '/home/bob/junk/imgtest/' );
-
+ImgManagerApp( $oApp, '/home/bob/junk/imgtest/',
+    [ 'imgmanlib' => [ 'fSizePercentThreshold' => 90.0,    // if filesize is reduced below this threshold, use the new file
+                       'bounding_box' => 1200,             // scale down to 1200x1200 if larger
+                       'jpg_quality' => 85
+    ]] );
 
 ?>
