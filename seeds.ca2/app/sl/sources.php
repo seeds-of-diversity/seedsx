2cf621f79955a177777ed56c54955a6eb6342a9a
bobwildfong <bob@seeds.ca>
2019-08-20 22:16:47 -0400

    move sources to seedapp

---
+++ seeds.ca2/app/sl/sources.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Seed Sources entry point
+ *
+ * Copyright (c) 2014-2019 Seeds of Diversity Canada
+ *
+ */
+
+if( !defined("SITEROOT") )  define("SITEROOT", "../../");
+include_once( SITEROOT."site.php" );
+include_once( SEEDAPP."sl/sources.php" );
