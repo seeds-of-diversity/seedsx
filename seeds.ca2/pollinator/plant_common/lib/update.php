5e0cf7dfef60a8c8b869e56787ef961f19d4c79a
bobwildfong <bob@seeds.ca>
2020-01-04 11:12:46 -0500

    use seedcore

--- seeds.ca2/pollinator/plant_common/lib/update.php
+++ seeds.ca2/pollinator/plant_common/lib/update.php
@@ -14,7 +14,7 @@ function Update()
 
             $dataPath = "../img/".$id;
             if( is_dir($dataPath) ) {
-                recursive_remove_directory( $dataPath );
+                SEEDCore_RemoveDirectory( $dataPath );
             }
         }
 
@@ -29,81 +29,6 @@ function Update()
     }
 }
 
-
-
-
-// to use this function to totally remove a directory, write:
-// recursive_remove_directory('path/to/directory/to/delete');
-
-// to use this function to empty a directory, write:
-// recursive_remove_directory('path/to/full_directory',TRUE);
-
-function recursive_remove_directory($directory, $empty=FALSE)
-{
-	// if the path has a slash at the end we remove it here
-	if(substr($directory,-1) == '/')
-	{
-		$directory = substr($directory,0,-1);
-	}
-
-	// if the path is not valid or is not a directory ...
-	if(!file_exists($directory) || !is_dir($directory))
-	{
-		// ... we return false and exit the function
-		return FALSE;
-
-	// ... if the path is not readable
-	}elseif(!is_readable($directory))
-	{
-		// ... we return false and exit the function
-		return FALSE;
-
-	// ... else if the path is readable
-	}else{
-
-		// we open the directory
-		$handle = opendir($directory);
-
-		// and scan through the items inside
-		while (FALSE !== ($item = readdir($handle)))
-		{
-			// if the filepointer is not the current directory
-			// or the parent directory
-			if($item != '.' && $item != '..')
-			{
-				// we build the new path to delete
-				$path = $directory.'/'.$item;
-
-				// if the new path is a directory
-				if(is_dir($path))
-				{
-					// we call this function with the new path
-					recursive_remove_directory($path);
-
-				// if the new path is a file
-				}else{
-					// we remove the file
-					unlink($path);
-				}
-			}
-		}
-		// close the directory
-		closedir($handle);
-
-		// if the option to empty is not set to true
-		if($empty == FALSE)
-		{
-			// try to delete the now empty directory
-			if(!rmdir($directory))
-			{
-				// return false if not possible
-				return FALSE;
-			}
-		}
-		// return success
-		return TRUE;
-	}
-}
 
 
 function getRAPost( $k )
@@ -174,6 +99,10 @@ function UpdateRecord( $kfdb, $id )
     $image_fruit   = str_replace("'","",$_FILES['img_fruit']['name']);
     $image_leaves  = str_replace("'","",$_FILES['img_leaves']['name']);
 
+    $dbPlantDesc  = SEEDInput_GetStrDB('plant_desc');
+    $dbScName     = SEEDInput_GetStrDB('sc_name');
+    $dbCommonName = SEEDInput_GetStrDB('common_name');
+
 
     if( $bUpdate ) {
         // Update
@@ -182,9 +111,9 @@ function UpdateRecord( $kfdb, $id )
                   .($image_fruit   ? ("image_fruit='".addslashes($image_fruit)."', ") : "")
                   .($image_leaves  ? ("image_leaves='".addslashes($image_leaves)."', ") : "");
 
-        $plant_desc_sql  = "info_text='".SEEDInput_GetStrDB('plant_desc')."' ";
-        $sc_name_sql     = "Scientific_Name='".SEEDInput_GetStrDB('sc_name')."', ";
-        $common_name_sql = "Common_Name='".SEEDInput_GetStrDB('common_name')."', ";
+        $plant_desc_sql  = "info_text='$dbPlantDesc' ";
+        $sc_name_sql     = "Scientific_Name='$dbScName', ";
+        $common_name_sql = "Common_Name='$dbCommonName', ";
 
 
         $bee_sql      = "Bee_Resource='".getRAPost('beeSelect')."', ";
@@ -218,9 +147,6 @@ function UpdateRecord( $kfdb, $id )
         }
     } else {
         // Insert
-        $plant_desc  = SEEDSafeGPC_GetStrDB('plant_desc');
-        $sc_name     = SEEDSafeGPC_GetStrDB('sc_name');
-        $common_name = SEEDSafeGPC_GetStrDB('common_name');
 
         $bee_sql      = getRAPost('beeSelect');
         $plant_sql    = getRAPost('plantSelect');
@@ -229,8 +155,8 @@ function UpdateRecord( $kfdb, $id )
 
         $id = $kfdb->InsertAutoInc( "INSERT into main (Scientific_Name,Common_Name,Bee_Resource,Season,Plant_Type,Location,"
                                                      ."image_flower,image_fruit,image_leaves,image_habitus,info_text,data) "
-                                   ."VALUES('$sc_name','$common_name','$bee_sql','$season_sql','$plant_sql','$location_sql',"
-                                          ."'$image_flower','$image_fruit','$image_leaves','$image_habitus','$plant_desc','')" );
+                                   ."VALUES('$dbScName','$dbCommonName','$bee_sql','$season_sql','$plant_sql','$location_sql',"
+                                          ."'$image_flower','$image_fruit','$image_leaves','$image_habitus','$dbPlantDesc','')" );
         if( $id ) {
             $ok = true;
         }
