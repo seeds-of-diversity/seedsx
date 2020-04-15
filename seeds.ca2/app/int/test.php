487025b669ad95254eb4252869986ae27a9d4241
bob <bob@localhost>
2020-04-15 16:26:31 -0400

    home page blocks

--- seeds.ca2/app/int/test.php
+++ seeds.ca2/app/int/test.php
@@ -41,8 +41,116 @@ $docrep_p = "<p>Your user id is [[SEEDSessionAccount_email:3]]</p>"
 
 $test = $oApp->sess->SmartGPC( 'test' );
 
+$sTmpHome = <<<TmpHome
+<style>
+.SoDHomeBlock {
+
+}
+.SoDHomeBlock01 {
+
+}
+.SoDHomeBlock02 {
+
+}
+.SoDHomeBlock .SoDHomeBlockImg {
+    position:relative;
+    overflow:hidden;
+    padding-bottom:80%;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size:cover;
+    margin:10px 0px;
+}
+.SoDHomeBlock .SoDHomeBlockCaption {
+    position:absolute;
+    bottom:0;
+    left:10%;
+    right:5%;
+    margin:0px auto 15px auto;
+    
+    color: white;
+    text-shadow: 1px 1px #888;
+}
+
+/* Screen < sm : all blocks are full width */
+.SoDHomeBlock01 .SoDHomeBlockCaption h4 { font-size:xx-large; font-weight:bold; }
+.SoDHomeBlock01 .SoDHomeBlockCaption    { font-size:x-large;   font-weight:bold; }
+.SoDHomeBlock02 .SoDHomeBlockCaption h4 { font-size:xx-large;   font-weight:bold; }
+.SoDHomeBlock02 .SoDHomeBlockCaption    { font-size:x-large;  font-weight:bold; }
+
+/* Screen > sm: block01 is four times bigger than block02 */
+@media only screen and (min-width : 768px){
+.SoDHomeBlock01 .SoDHomeBlockCaption h4 { font-size:x-large; font-weight:bold; }
+.SoDHomeBlock01 .SoDHomeBlockCaption    { font-size:large;   font-weight:bold; }
+.SoDHomeBlock02 .SoDHomeBlockCaption h4 { font-size:large;   font-weight:bold; }
+.SoDHomeBlock02 .SoDHomeBlockCaption    { font-size:medium;  font-weight:bold; }
+}
+
+.SoDHomeBlock a {
+    text-decoration:none;
+}
+</style>
+
+<div class='container-fluid' style='margin:0px auto;width:90%'>
+    <div class='row'>
+        <div class='col-sm-6'>
+            <div class='SoDHomeBlock SoDHomeBlock01'>
+            <a href='A'>
+            <div class='SoDHomeBlockImg' style='background-image: url("../../../../docrep_upload1/sfile/01.jpg");'>
+                <div class='SoDHomeBlockCaption'><h4>Walk With a Horse</h4>It's good for your heart!</div>
+            </div>
+            </a>
+            </div>
+        </div>
+        <div class='col-sm-6'>
+            <div class='row'>    
+                <div class='col-sm-6'>
+                    <div class='SoDHomeBlock SoDHomeBlock02'>
+                    <a href='B'>
+                    <div class='SoDHomeBlockImg' style='background-image: url("../../../../docrep_upload1/sfile/02.jpg');">
+                        <div class='SoDHomeBlockCaption'><h4>Learn How to Grow Beans</h4>They're easier than you think!</div>
+                    </div>
+                    </a>
+                    </div>
+                </div>
+                <div class='col-sm-6'>
+                    <div class='SoDHomeBlock SoDHomeBlock02'>
+                    <a href='C'>
+                    <div class='SoDHomeBlockImg' style='background-image: url("../../../../docrep_upload1/sfile/03.jpg');">
+                        <div class='SoDHomeBlockCaption'><h4>Learn How to Grow Flowers</h4>They're very pretty!</div>
+                    </div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <div class='row'>    
+                <div class='col-sm-6'>
+                    <div class='SoDHomeBlock SoDHomeBlock02'>
+                    <a href='D'>
+                    <div class='SoDHomeBlockImg' style='background-image: url("../../../../docrep_upload1/sfile/04.jpg');">
+                        <div class='SoDHomeBlockCaption'><h4>More Flowers</h4>You can never have enough!</div>
+                    </div>
+                    </a>
+                    </div>
+                </div>
+                <div class='col-sm-6'>
+                    <div class='SoDHomeBlock SoDHomeBlock02'>
+                    <a href='E'>
+                    <div class='SoDHomeBlockImg' style='background-image: url("../../../../docrep_upload1/sfile/05.jpg');">
+                        <div class='SoDHomeBlockCaption'><h4>Learn How to Make Pickles</h4>What completes a sandwich better?</div>
+                    </div>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+TmpHome;
+
+
 switch( $test ) {
-    case 'home-en':    $s .= $oTmpl->ExpandStr( "[[SEEDContent:home-en]]" );    break;
+    case 'home-en':    $s .= $oTmpl->ExpandStr( $sTmpHome, ['lang'=>'EN'] );    break;
     case 'home-fr':    $s .= $oTmpl->ExpandStr( "[[SEEDContent:home-fr]]" );    break;
     case 'home-edit':  $s .= $oTmpl->ExpandStr( "[[SEEDContent:home-edit]]" );  break;
 
