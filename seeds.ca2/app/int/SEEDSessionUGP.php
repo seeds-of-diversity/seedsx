a08f5889419e3ed8addd82c9eb50c2bf35a6af05
bob <bob@localhost>
2020-05-21 21:56:53 -0400

    UGP code accessible via seedapp

---
+++ seeds.ca2/app/int/SEEDSessionUGP.php
@@ -0,0 +1,7 @@
+<?php
+
+if( !defined("SITEROOT") )  define("SITEROOT", "../../");
+include_once( SITEROOT."site.php" );
+
+define("APP_SEEDUGP_DB",'seeds1');
+include_once( SEEDAPP."app_SEEDUGP.php" );
