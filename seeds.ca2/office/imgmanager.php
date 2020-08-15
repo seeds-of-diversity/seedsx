474a53dd81e56518c6ee67d97ea5fd6a0a10f70a
bob <bob@localhost>
2020-08-14 20:26:09 -0400

    seedapp imgmanager improved

--- seeds.ca2/office/imgmanager.php
+++ seeds.ca2/office/imgmanager.php
@@ -2,16 +2,9 @@
 
 if( !defined("SITEROOT") )  define("SITEROOT", "../");
 include_once( SITEROOT."site2.php" );
+
+$rootdir = '/home/bob/junk/imgtest/';
+
 include_once( SEEDAPP."imgman/ImgManager.php" );
-
-$oApp = new SEEDAppConsole( $config_KFDB['seeds2'] + [ 'sessPermsRequired' => array(), //  'imgman' => 'W' ),
-                                                       'logdir' => SITE_LOG_ROOT ]
-);
-
-ImgManagerApp( $oApp, '/home/bob/junk/imgtest/',
-    [ 'imgmanlib' => [ 'fSizePercentThreshold' => 90.0,    // if filesize is reduced below this threshold, use the new file
-                       'bounding_box' => 1200,             // scale down to 1200x1200 if larger
-                       'jpg_quality' => 85
-    ]] );
 
 ?>
