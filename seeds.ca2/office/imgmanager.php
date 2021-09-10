c0bce002a52de8f9e757e5c019f51308773aea11
bobwildfong <bob@seeds.ca>
2021-09-10 16:08:38 -0400

    make targetExt a config option

--- seeds.ca2/office/imgmanager.php
+++ seeds.ca2/office/imgmanager.php
@@ -3,8 +3,9 @@
 if( !defined("SITEROOT") )  define("SITEROOT", "../");
 include_once( SITEROOT."site2.php" );
 
-$rootdir = '/home/bob/junk/imgtest/';
+$raConfig = [
+    'rootdir' => '/home/bob/junk/imgtest/',
+    'imgmanlib' => ['targetExt'=>'jpg']
+];
 
 include_once( SEEDAPP."imgman/ImgManager.php" );
-
-?>
