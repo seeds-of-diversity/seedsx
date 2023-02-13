75a2a59041a1a1e06497528efaa1254465f070d4
bobwildfong <bob@seeds.ca>
2023-02-13 16:20:22 -0500

    rename app

--- seeds.ca2/office/mbr/mbr_email.php
+++ seeds.ca2/office/mbr/mbr_email.php
@@ -2,4 +2,4 @@
 
 define( "SITEROOT", "../../" );
 include_once( SITEROOT."site2.php" );
-include_once( SEEDAPP."mbr/mbrMailingList_app.php" );
+include_once( SEEDAPP."mbr/app_mbrMailingList.php" );
