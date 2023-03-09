00c13a21a523a143e9868a866568c25818c77323
bobwildfong <bob@seeds.ca>
2023-03-09 15:17:19 -0500

    new growouts tool

---
+++ seeds.ca2/app/sl/growouts.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Seed Growouts entry point
+ *
+ * Copyright (c) 2023 Seeds of Diversity Canada
+ *
+ */
+
+if( !defined("SITEROOT") )  define("SITEROOT", "../../");
+include_once( SITEROOT."site.php" );
+include_once( SEEDAPP."sl/app_growouts.php" );
