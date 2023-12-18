e8e0790ae930b0bf9330b07f54b601178067a905
bobwildfong <bob@seeds.ca>
2023-12-18 18:51:39 -0500

    change UI from console01 to console02

--- seeds.ca2/app/mbr/msd-edit.php
+++ seeds.ca2/app/mbr/msd-edit.php
@@ -28,9 +28,13 @@ include_once( SEEDAPP."seedexchange/msdedit.php" );
 include_once( SEEDAPP."seedexchange/msdadmin.php" );
 include_once( SEEDLIB."msd/msdlib.php" );
 
-list($kfdb, $sess, $lang) = SiteStartSessionAccount( ["W sed"] );
 
-$oApp = SEEDConfig_NewAppConsole( ['db'=>'seeds1', 'sessPermsRequired' => ["W sed"], 'lang' => $lang ] );
+$oApp = SEEDConfig_NewAppConsole( ['db'=>'seeds1',
+                                   'sessPermsRequired' => ["|", "W MSD", "W MSDOffice",
+                                                                "W sed" ],                  // deprecate
+                                   'lang' => site_define_lang() ] );
+list($kfdb, $sess, $langDummy) = SiteStartSessionAccountNoUI( [] );
+$lang = $oApp->lang;
 
 SEEDPRG();
 
@@ -73,6 +77,7 @@ class SEDMbrGrower extends MSEEditAppTabGrower
 }
 
 
+/*
 class MyConsole extends Console01
 {
     public  $oApp;
@@ -83,13 +88,13 @@ class MyConsole extends Console01
     private $kCurrSpecies;
     public $oMSDLib;
 
-    function __construct( SEDMbr $oSed, SEEDAppConsole $oApp, $raParms )
+    function __construct( SEDMbr $oSed, SEEDAppConsole $oApp, MSDLib $oMSDLib, $raParms )
     {
         $this->oSed = $oSed;
         $this->oApp = $oApp;
         parent::__construct( $oSed->kfdb, $oSed->sess, $raParms );
 
-        $this->oMSDLib = new MSDLib( $oApp );
+        $this->oMSDLib = $oMSDLib;
 
         if( $this->oMSDLib->PermOfficeW() ) {
             $this->oSed->bOffice = true;
@@ -121,6 +126,13 @@ class MyConsole extends Console01
                 break;
             case 'Office':
                 $this->oW = new MSDAdminTab( $this->oMSDLib );
+                break;
+
+            case 'Archive':
+//select A.year,nGrowers,nSeeds, nSeeds/nGrowers
+//  from (select year,count(*) as nGrowers from sed_growers group by 1) as A,
+//       (select year,count(*) as nSeeds from sed_seeds group by 1) as B
+//  where A.year=B.year order by 1;
                 break;
         }
     }
@@ -173,44 +185,125 @@ class MyConsole extends Console01
         return( $s );
     }
 }
+*/
 
 
+$oMSDLib = new MSDLib( $oApp );
 $oSed = new SEDMbr( $kfdb, $sess, $lang );
 //$oSed->update();
 
-$raConsoleParms = array(
-    'HEADER' => $oSed->S("MSD title"),
-    'CONSOLE_NAME' => "msdedit",
-//    'HEADER_LINKS' => array( array( 'href' => 'mbr_email.php',    'label' => "Email Lists",  'target' => '_blank' ),
-//                             array( 'href' => 'mbr_mailsend.php', 'label' => "Send 'READY'", 'target' => '_blank' ) ),
-
-    'TABSETS' => array( "main" => array( 'tabs' => array( 'Growers' => array( 'label' => $oSed->S("Tab G") ),
-                                                          'Seeds'   => array( 'label' => $oSed->S("Tab S") ),
-                                                          'Edit'    => array( 'label' => "Edit" ),
-                                                          'Office'  => array( 'label' => "Office" ) ) ) ),
-    'lang' => $lang,
-    'bBootstrap' => true,
-    'css_files' => [ W_CORE."css/console02.css" ],
-    'script_files' => [ W_ROOT."std/js/SEEDStd.js", W_CORE."js/SEEDCore.js", W_CORE."js/console02.js",W_ROOT."std/js/SEEDPopover.js" ],
-    'sCharset' => 'utf-8'
-);
-$oC = new MyConsole( $oSed, $oApp, $raConsoleParms );
-
-if( !$oC->klugeThisPageIsUTF8 ) {
-    $oC->SetConfig( ['sCharset' => 'cp1252'] );
-}
-
-// Output reports in this window with no console.
-// MSDLibReport sets header(charset) based on the format of report
-if( $oC->oMSDLib->PermOfficeW() && SEEDInput_Str('doReport') ) {
+/* Output reports in this window with no console.
+ * MSDLibReport sets header(charset) based on the format of report
+ */
+if( $oMSDLib->PermOfficeW() && SEEDInput_Str('doReport') ) {
     include_once( SEEDLIB."msd/msdlibReport.php" );
-    echo (new MSDLibReport($oC->oMSDLib))->Report();
+    echo (new MSDLibReport($oMSDLib))->Report();
     exit;
 }
 
-echo $oC->DrawConsole( $oSed->SEDStyle()."[[TabSet: main]]" );
 
-?>
+class MyConsole02TabSet extends Console02TabSet
+{
+    private $oApp;
+    private $oMSDLib;
+    private $oW = null;
+    private $oContacts;
+
+    function __construct(SEEDAppConsole $oApp, MSDLib $oMSDLib)
+    {
+        global $consoleConfig;
+        parent::__construct( $oApp->oC, $consoleConfig['TABSETS'] );
+
+        $this->oApp = $oApp;
+        $this->oMSDLib = $oMSDLib;
+        //$this->oContacts = new Mbr_Contacts( $oApp );
+
+        if( $this->oMSDLib->PermOfficeW() ) {
+//            $this->oSed->bOffice = true;
+
+            $this->kCurrGrower = $this->oApp->oC->oSVA->SmartGPC( 'selectGrower', [0] );
+        } else {
+            $this->kCurrGrower = $this->oApp->sess->GetUID();
+            $this->kCurrSpecies = 0;   // all species
+        }
+        $this->kCurrSpecies = $this->oApp->oC->oSVA->SmartGPC( 'selectSpecies', [0] );    // normally an int, but can be tomatoAC, tomatoDH, etc
+    }
+
+    function TabSetPermission( $tsid, $tabname )
+    {
+        switch( $tabname ) {
+            case 'growers':
+            case 'seeds':
+                return( Console02TabSet::PERM_SHOW );
+            case 'edit':
+            case 'office':
+            case 'archive':
+                return( $this->oMSDLib->PermOfficeW() ? Console02TabSet::PERM_SHOW : Console02TabSet::PERM_HIDE );
+        }
+        return( Console02TabSet::PERM_HIDE );
+    }
+
+    function TabSet_main_growers_Init()           { $this->oW = new MSEEditAppTabGrower($this->oApp); $this->oW->Init_Grower($this->kCurrGrower); }
+    function TabSet_main_growers_ControlDraw()    { return( $this->oW->ControlDraw_Grower() ); }
+    function TabSet_main_growers_ContentDraw()    { return( $this->oW->ContentDraw_Grower() ); }
+
+    function TabSet_main_seeds_Init()             { $this->oW = new MSEEditAppTabSeeds($this->oApp); $this->oW->Init_Seeds($this->kCurrGrower, $this->kCurrSpecies); }
+    function TabSet_main_seeds_ControlDraw()      { return( $this->oW->ControlDraw_Seeds() ); }
+    function TabSet_main_seeds_ContentDraw()      { return( $this->oW->ContentDraw_Seeds() ); }
+
+    function TabSet_main_edit_Init()              { $this->oW = new MSDOfficeEditTab( $this->oMSDLib ); }
+    function TabSet_main_edit_ControlDraw()       { return( $this->oMSDLib->PermOfficeW() ? $this->oW->DrawControl() : "" ); }         // MSDOfficeEditTab or MSDAdminTab
+    function TabSet_main_edit_ContentDraw()       { return( $this->oMSDLib->PermOfficeW() ? $this->oW->DrawContent() : "" ); }
+
+    function TabSet_main_office_Init()            { $this->oW = new MSDAdminTab( $this->oMSDLib ); }
+    function TabSet_main_office_ControlDraw()     { return( $this->oMSDLib->PermOfficeW() ? $this->oW->DrawControl() : "" ); }         // MSDOfficeEditTab or MSDAdminTab
+    function TabSet_main_office_ContentDraw()     { return( $this->oMSDLib->PermOfficeW() ? $this->oW->DrawContent() : "" ); }
+
+    function TabSet_main_archive_Init()           {
+//select A.year,nGrowers,nSeeds, nSeeds/nGrowers
+//  from (select year,count(*) as nGrowers from sed_growers group by 1) as A,
+//       (select year,count(*) as nSeeds from sed_seeds group by 1) as B
+//  where A.year=B.year order by 1;
+    }
+}
+
+$consoleConfig = [
+    'CONSOLE_NAME' => "msd-edit",
+    'HEADER' =>  $oSed->S("MSD title"),
+    'TABSETS' => ['main'=> ['tabs' => [ 'growers' => ['label' => $oMSDLib->oTmpl->ExpandTmpl("MSEEdit_tablabel_G")],
+                                        'seeds'   => ['label' => $oMSDLib->oTmpl->ExpandTmpl("MSEEdit_tablabel_S")],
+                                        'edit'    => ['label' => "Edit"],
+                                        'office'  => ['label' => "Office"],
+                                        'archive' => ['label' => "Archive"],
+                                      ],
+
+                            'perms' => ['growers' => Console02TabSet::PERM_SHOW,
+                                        'seeds'   => Console02TabSet::PERM_SHOW,
+                                        'edit'    => $oMSDLib->PermOfficeW() ? Console02TabSet::PERM_SHOW : Console02TabSet::PERM_HIDE,
+                                        'office'  => $oMSDLib->PermOfficeW() ? Console02TabSet::PERM_SHOW : Console02TabSet::PERM_HIDE,
+                                        'archive' => $oMSDLib->PermOfficeW() ? Console02TabSet::PERM_SHOW : Console02TabSet::PERM_HIDE,
+                                       ]
+                           ],
+                 ],
+    'urlLogin'=>'../login/',
+
+    'consoleSkin' => 'green',
+
+];
+$oApp->oC->SetConfig($consoleConfig);
+
+$oCTS = new MyConsole02TabSet( $oApp, $oMSDLib );
+// kluge to set sCharset based on current tab
+// Growers and Office are cp1252, but make sure '' is too. Growers was being rendered in utf-8 on initialization, which led some members to enter notes in that charset.
+$oApp->oC->SetConfig(['sCharset', $oCTS->TabSetGetCurrentTab('main') == 'seeds' ? 'utf-8' : 'cp1252']);
+//var_dump($oCTS->TabSetGetCurrentTab('main') == 'seeds' ? 'utf-8' : 'cp1252');
+
+$sBody = $oApp->oC->DrawConsole( "[[TabSet:main]]", ['oTabSet'=>$oCTS] );
+//echo $oSed->SEDStyle().$sBody;
+//echo Console02Static::HTMLPage( utf8_encode($sBody), "", 'EN', ['consoleSkin'=>'green'] );
+
+
+$sHead = <<<HEAD
 <style>
 .popover { width: 20%; }    /* when container:'body' specified ths makes the popover 20% of the window width */
 </style>
@@ -219,7 +312,7 @@ $(document).ready( function() {
     SEEDPopover();
 });
 
-SEEDPopover_Def = {
+var SEEDPopover_Def = {
 	mbr_code:
 	    { placement:'right', trigger: 'hover', container: 'body',
 	      title:   "Member Code",
@@ -243,3 +336,37 @@ SEEDPopover_Def = {
 };
 
 </script>
+HEAD;
+
+
+echo Console02Static::HTMLPage( $sBody.$oSed->SEDStyle(), $sHead,
+                                $oApp->lang,
+                                ['consoleSkin'=>'green',
+                                 'raScriptFiles'=>[ W_ROOT."std/js/SEEDStd.js", W_CORE."js/SEEDCore.js", W_CORE."js/console02.js",W_CORE."js/SEEDPopover.js" ]
+                                ] );
+
+/*
+$raConsoleParms = array(
+    'HEADER' => $oSed->S("MSD title"),
+    'CONSOLE_NAME' => "msdedit",
+//    'HEADER_LINKS' => array( array( 'href' => 'mbr_email.php',    'label' => "Email Lists",  'target' => '_blank' ),
+//                             array( 'href' => 'mbr_mailsend.php', 'label' => "Send 'READY'", 'target' => '_blank' ) ),
+
+    'TABSETS' => array( "main" => array( 'tabs' => array( 'Growers' => array( 'label' => $oMSDLib->oTmpl->ExpandTmpl("MSEEdit_tablabel_G") ),
+                                                          'Seeds'   => array( 'label' => $oMSDLib->oTmpl->ExpandTmpl("MSEEdit_tablabel_S") ),
+                                                          'Edit'    => array( 'label' => "Edit" ),
+                                                          'Office'  => array( 'label' => "Office" ) ) ) ),
+    'lang' => $lang,
+    'bBootstrap' => true,
+    'css_files' => [ W_CORE."css/console02.css" ],
+    'script_files' => [ W_ROOT."std/js/SEEDStd.js", W_CORE."js/SEEDCore.js", W_CORE."js/console02.js",W_CORE."js/SEEDPopover.js" ],
+    'sCharset' => 'utf-8'
+);
+$oC = new MyConsole( $oSed, $oApp, $oMSDLib, $raConsoleParms );
+
+if( !$oC->klugeThisPageIsUTF8 ) {
+    $oC->SetConfig( ['sCharset' => 'cp1252'] );
+}
+
+echo $oC->DrawConsole( "[[TabSet: main]]" );
+*/
