b7dcd2b15a030ebea9bc53c0b11c4cc2bafd7e3e
bobwildfong <bob@seeds.ca>
2024-12-14 11:56:01 -0500

    rename seedapp file

--- seeds.ca2/office/mbr/mbrPrint.php
+++ seeds.ca2/office/mbr/mbrPrint.php
@@ -4,6 +4,6 @@ define( "SITEROOT", "../../" );
 include_once( SITEROOT."site2.php" );
 //include_once( SEEDCOMMON."console/console01.php" );
 include_once( SEEDCOMMON."mbr/mbrCommon.php" );
-include_once( SEEDAPP."mbr/mbrPrint.php" );
+include_once( SEEDAPP."mbr/mbrPrint/app_mbrPrint.php" );
 
 
