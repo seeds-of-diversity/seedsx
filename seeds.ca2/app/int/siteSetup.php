566f6fe76cf8fdeb5198d751c72b8c0c1f3fd308
bob <bob@localhost>
2025-05-21 16:34:43 -0400

    fix syntax

--- seeds.ca2/app/int/siteSetup.php
+++ seeds.ca2/app/int/siteSetup.php
@@ -1,9 +1,7 @@
 <?php
 
-define( SITEROOT, "../../" );
+define( "SITEROOT", "../../" );
 require( SITEROOT."site.php");
 require(SEEDCOMMON."siteSetup.php");
 
 SiteSetup( "seeds" ) or die();    // seedcommon, sets up all the common stuff like SEEDSession, SEEDPerms, DocRep
-
-?>
